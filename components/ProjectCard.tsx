5f38d52457b811f63b734f28090d937d17a20166
wesngu28 <serpents123@gmail.com>
2022-09-27 13:15:53 -0700

    add projects and resize their img and mobile style

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -7,19 +7,19 @@ interface Props {
 
 export default function ProjectCard(props: Props) {
     return (
-        <li className="bg-[#21222a] m-1 inline-block w-[30%]">
-            <img className="w-full" src={props.image} />
+        <li className="p-4 bg-[#21222a] m-4 inline-block">
+            <img src={props.image} />
             <div className={`pr-2 pl-2`}>
                 <ul className={'flex mt-2 justify-center flex-wrap'}>
                     {props.techs.map((arr, i) => {
                         return (
-                            <li className="ml-2 mr-2 p-1 bg-[beige] text-black rounded-lg">
+                            <li className="m-2 p-1 bg-[beige] text-black rounded-lg">
                                 <p>{arr}</p>
                             </li>
                         );
                     })}
                 </ul>
-                <h2 className="m-2">{props.name}</h2>
+                <h2 className="m-2 text-center font-bold text-lg">{props.name}</h2>
                 <p>{props.desc}</p>
             </div>
         </li>
