172f6b2fc95d97b1baa59ff1b049606efde89024
wesngu28 <serpents123@gmail.com>
2022-09-27 13:21:27 -0700

    combine overflow about to just about

--- components/About.tsx
+++ components/About.tsx
@@ -1,18 +1,25 @@
-export default function About() {
+interface Props {
+    header: string;
+    paragraph1: string;
+    paragraph2?: string;
+    img: string;
+}
+
+export default function About({header, paragraph1, paragraph2, img}: Props) {
     return (
         <div className="mt-11 flex justify-center items-center p-4 bg-[#1F2022] w-[75vw] rounded-3xl">
             <div className="p-4 w-[55vw]">
                 <h1 className="text-center text-3xl font-bold">
-                    about me
+                    {header}
                 </h1>
                 <p className="p-4 text-left text-xl leading-8">
-                    Hello, my name is Wesley. I am a student at the University of Washington studying geography and data science. I've always been passionate with computers and after taking AP Computer Science during sophomore year I knew that I wanted to work in a field even remotely related to programming.
+                    {paragraph1}
                 </p>
                 <p className="p-4 text-left text-xl leading-8">
-                    Besides school, I enjoy listening to music (favorite genres being progressive house (a la deadmau5), j-rock, among others), as well as playing games (if you consider modding skyrim in an infinite loop without launching the game a game). I also have two dogs which you can see pictures of here.
+                    {paragraph2}
                 </p>
             </div>
-            <img className="p-4 rounded-full mx-auto w-1/5 h-1/4" src="/grotesque.jpeg" />
+            <img className="p-4 rounded-full mx-auto w-1/5 h-1/4" src={img} />
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   return (
     <div id="wrapper" className="border-red-400 border flex flex-col justify-center items-center">
       <Nav fourth={'about'}/>
-      <About />
+      <About header='about me' img='/grotesque.jpeg' paragraph2='Besides school, I enjoy listening to music (favorite genres being progressive house (a la deadmau5), j-rock, among others), as well as playing games (if you consider modding skyrim in an infinite loop without launching the game a game). I also have two dogs which you can see pictures of here.'paragraph1='Hello, my name is Wesley. I am a student at the University of Washington studying geography and data science. I have always been passionate with computers and after taking AP Computer Science during sophomore year I knew that I wanted to work in a field even remotely related to programming.'/>
       <Skills />
       <Projects />
       <Footer />
