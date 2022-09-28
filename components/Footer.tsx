746dd37d35a6c5b8289f3b12a18e02083330a7d3
wesngu28 <serpents123@gmail.com>
2022-09-27 22:47:19 -0700

    footer adjustments

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
     return (
-        <footer className='bg-[#21222a] pl-4 pr-4 relative bottom-0 mt-11'>
-            <p>Made with Tailwind, React, and Next.js.</p>
+        <footer className='bg-[#21222a] p-8 relative bottom-0 mt-11 w-full'>
+            <p className="text-center">Made with Tailwind, React, and Next.js.</p>
+            <p className="mt-3 text-center">Hosted by Vercel.</p>
+            <p className="mt-3 text-center">Wesley Nguyen 2022</p>
         </footer>
     )
 }
