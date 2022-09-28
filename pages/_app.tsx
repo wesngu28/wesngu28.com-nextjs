3ffb6900396e4c18ac36bcdb43df0f38a5ad7361
wesngu28 <serpents123@gmail.com>
2022-09-28 03:11:34 -0700

    head and favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/about.tsx
+++ pages/about.tsx
@@ -5,10 +5,12 @@ import Games from '../components/Games'
 import Music from '../components/Music'
 import About from '../components/About'
 import Footer from '../components/Footer'
+import Metadata from '../components/Metadata'
 
 const AboutPage: NextPage = () => {
   return (
     <div id="wrapper" className= "flex flex-col justify-center items-center">
+      <Metadata />
       <About header='even more about me' paragraph1='If you clicked on this for whatever reason, have fun learning more about me than you should really know about me.' destination='/'/>
       <Music />
       <Games />
