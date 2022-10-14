75960d18a008c718126d99964b96d266d887c382
wesngu28 <serpents123@gmail.com>
2022-10-14 01:05:44 -0700

    show language bar for projects

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,33 +1,92 @@
-import Image, { StaticImageData } from "next/image"
+import languageList from '../languages.json'
+import Image, { StaticImageData } from 'next/image'
+import useSWR from 'swr'
 
 interface Props {
-    techs: string[]
-    image: StaticImageData
-    name: string
-    desc: string
-    url: string
-    live?: string
+  techs: string[]
+  image: StaticImageData
+  name: string
+  desc: string
+  url: string
+  live?: string
 }
 
 export default function ProjectCard({ techs, image, name, desc, url, live }: Props) {
-    return (
-        <li className="p-4 bg-[#21222a] m-4 inline-block">
-            {live ? <a className="m-auto" target="_blank" href={live} rel="noopener noreferrer">
-                <Image alt={`${name} screenshot or related content if no screenshot makes sense`} src={image} />
-            </a> : <Image alt={`${name} screenshot or related content if no screenshot makes sense`} src={image} />}
-            <div className={`pr-2 pl-2`}>
-                <ul className={'flex mt-2 justify-center flex-wrap'}>
-                    {techs.map((arr, i) => {
-                        return (
-                            <li key={arr} className="m-2 p-1 bg-[beige] text-black rounded-lg">
-                                <p>{arr}</p>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <a target="_blank" href={url} rel="noopener noreferrer"><h2 className="hover:underline m-2 text-center font-bold text-lg">{name}</h2></a>
-                <p>{desc}</p>
-            </div>
-        </li>
+  const swrFetch = async (url: string) => {
+    const linguist = await fetch(url)
+    const langs = await linguist.json()
+    const counts: Array<number> = Object.values(langs)
+    const total = counts.reduce(
+      (previousValue: number, currentValue) => previousValue + currentValue,
+      0
     )
+    Object.keys(langs).map(extension => {
+      langs[extension] = (langs[extension] / total) * 100
+    })
+    return langs
+  }
+
+  const { data, error } = useSWR(
+    `/api/github?repo=${url.replace('https://github.com/wesngu28/', '')}`,
+    swrFetch,
+    {
+      refreshInterval: 86400000,
+      revalidateOnFocus: false,
+    }
+  )
+
+  return (
+    <li className="p-4 bg-[#21222a] m-4 inline-block">
+      {live ? (
+        <a className="m-auto" target="_blank" href={live} rel="noopener noreferrer">
+          <Image
+            alt={`${name} screenshot or related content if no screenshot makes sense`}
+            src={image}
+          />
+        </a>
+      ) : (
+        <Image
+          alt={`${name} screenshot or related content if no screenshot makes sense`}
+          src={image}
+        />
+      )}
+      <div className="-my-2 w-full">
+        {data
+          ? Object.keys(data).map(lang => {
+              const divStyle = {
+                width: `${data[lang]}%`,
+                backgroundColor: `${(languageList as any)[lang].color}`,
+                color: `${(languageList as any)[lang].color}`,
+              }
+              return (
+                <span
+                  title={lang}
+                  key={lang}
+                  style={divStyle}
+                  className={`inline-block bg-[#3178c6] text-[0.4rem]`}
+                  data-view-component="true"
+                >
+                  .
+                </span>
+              )
+            })
+          : null}
+      </div>
+      <div className={`pr-2 pl-2`}>
+        <ul className={'flex mt-1 justify-center flex-wrap'}>
+          {techs.map((arr, i) => {
+            return (
+              <li key={arr} className="m-2 p-1 bg-[beige] text-black rounded-lg">
+                <p>{arr}</p>
+              </li>
+            )
+          })}
+        </ul>
+        <a target="_blank" href={url} rel="noopener noreferrer">
+          <h2 className="hover:underline m-2 text-center font-bold text-lg">{name}</h2>
+        </a>
+        <p>{desc}</p>
+      </div>
+    </li>
+  )
 }
---
+++ pages/api/github.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const { GITHUB_PERSONAL_TOKEN: githubToken } = process.env
+
+const github = async (req: NextApiRequest, res: NextApiResponse) => {
+  const repoName = req.query.repo
+  const response = await fetch(
+    `https://api.github.com/repos/wesngu28/${repoName}/languages`,
+    {
+      headers: {
+        authorization: `token ${githubToken}`,
+      },
+    }
+  )
+
+  const languages = await response.json()
+  const langLines: Array<number> = Object.values(languages)
+  const lines = langLines.reduce((a, b) => a + b, 0)
+  Object.keys(languages).forEach(language => {
+    languages[language] = 100 * (languages[language] / lines)
+  })
+  console.log(languages)
+  return res.status(200).json(languages)
+}
+
+export default github
